b1c266dc131ec2d7a5951650bf7117e6e774ec60
Albin Kerouanton <albin.kerouanton@knplabs.com>
2019-09-26 14:04:05 +0200

    Init repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="kitipy",
+    version="0.1",
+    description="KNP task runner to automate and ease dev and ops workflows",
+    license="MIT",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+)
