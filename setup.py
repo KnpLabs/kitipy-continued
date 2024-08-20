e2cc46fa747965ea67d58bb704032b149e36147d
Swanoo <91881914+Swanoo@users.noreply.github.com>
2024-08-20 14:25:45 +0200

    chore: Downgrade boto3

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ import setuptools
 
 setuptools.setup(
     name="kitipy",
-    version="0.2",
+    version="0.3",
     description="KNP task runner to automate and ease dev and ops workflows",
     license="MIT",
     packages=setuptools.find_packages(),
     install_requires=[
-        "boto3-stubs[ecr]==1.35.1",
-        "boto3-stubs[ecs]==1.35.1",
-        "boto3-stubs[secretsmanager]==1.35.1",
-        "boto3==1.35.1",
+        "boto3-stubs[ecr]==1.34.1",
+        "boto3-stubs[ecs]==1.34.1",
+        "boto3-stubs[secretsmanager]==1.34.1",
+        "boto3==1.34.1",
         "click==7.1.2",
         "Jinja2==3.1.4",
         "paramiko==3.4.1",
